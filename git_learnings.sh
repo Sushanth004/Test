e18013d90d81f0e6ed5104d5231b714b81c006d5
Sushanth <Sushanth@gmail.com>
2022-11-28 20:28:14 +0530

    added test file

---
+++ git_learnings.sh
@@ -0,0 +1,9 @@
+git init # Initializes the empty repository
+git add <path> # Adds files to the staging area
+git commit -m "commit message" # Adds files to the repository
+
+git log # Shows the Commits
+git log --all # Shows all the Commits 
+
+git checkout <commit-id> # switches to the commitid mentioned
+git rm --cached <filenames> # unstages the file
